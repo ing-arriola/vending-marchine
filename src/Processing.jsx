742443ea2bb66dac0923245109777ff203cc5b8f
Jaime <ingeniero.arriola@gmail.com>
2021-04-26 18:26:07 -0600

    Deleted unnecesary query to backend and moved to a global state

--- src/Processing.jsx
+++ src/Processing.jsx
@@ -1,31 +1,15 @@
-import React,{ useEffect, useState } from 'react'
-import axios from 'axios'
+import React from 'react'
 import TableProcessing from './TableProcessing'
+import {useStoreon} from 'storeon/react'
 
 
 const Processing = ({state}) => {
-    const [products,setProducts] = useState([])
-    const [onloading,setOnloading] = useState(true)
-
-    const getProducts = () => {
-        const dataFirebase = axios.create({baseURL: process.env.REACT_APP_FIREBASE_URL})
-        dataFirebase.get('/processing.json')
-        .then(res => {
-            const prods = []
-            for (const key in res.data) {
-                prods.push({...res.data[key],id: key})
-            }
-            setProducts(prods)
-            })
-            setOnloading(false)
-    }
-
-    useEffect(() => getProducts(),[])
+    const {processing} = useStoreon('processing')
+    console.log(processing)    
     return (
         <>
             <TableProcessing 
-                loading={onloading}
-                products={products.filter(product => product.state === state)} 
+                products={processing.filter(product => product.state === state)} 
                 state={state} />  
         </>
     )
