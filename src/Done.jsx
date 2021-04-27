871c8de991dc9280b494aaf7bc4b313c86e571b0
Jaime <ingeniero.arriola@gmail.com>
2021-04-26 18:25:06 -0600

    Dispatching a event to global state to update products currently on process

--- src/Done.jsx
+++ src/Done.jsx
@@ -1,15 +1,29 @@
 import React,{ useEffect } from 'react'
+import {useStoreon} from 'storeon/react'
 import axios from 'axios'
 
+
 const Done = ({product}) => {
-    useEffect(() => {
+    const {dispatch} = useStoreon('tasks')
+
+    const competed = (res,id) => {
+        dispatch('completed',{...res,id})
+    }
+
+
+    const updateData = async () => {
         const dataFirebase = axios.create({baseURL: process.env.REACT_APP_FIREBASE_URL})
-        dataFirebase.put(`/processing/${product.id}.json`,{
+        const res = await dataFirebase.put(`/processing/${product.id}.json`,{
             minutes:product.minutes,
             seconds:product.seconds,
             name:product.name,
             time:product.time,
             state:2})
+        competed(res.data,product.id)
+    }
+
+    useEffect(() => {
+        updateData()
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     return (
