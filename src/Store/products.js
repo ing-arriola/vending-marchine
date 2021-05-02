398d3208ef157748ec7ecbe15eb769370dc93907
Jaime <ingeniero.arriola@gmail.com>
2021-05-02 11:55:17 -0600

    Created  a file to keep track of products changes

---
+++ src/Store/products.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+let products = store => {
+    store.on('@init',() => {
+        const prods = []
+        const dataFirebase = axios.create({baseURL: process.env.REACT_APP_FIREBASE_URL})
+        dataFirebase.get('/products.json')
+            .then(res => {
+                for (const key in res.data) {
+                    prods.push({...res.data[key],id: key})    
+                    }
+                }
+            )
+            return  {products:prods}    
+    })
+    store.on('newProduct',({products},order)=>{
+        return{
+            processing: [...products, order]
+        }
+    })
+}
+
+export default products
